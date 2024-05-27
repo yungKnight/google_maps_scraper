1817fa02aa8ed22e505251f3897704c67a20361b
yungKnight <badoknight1@gmail.com>
2024-05-27 16:26:39 +0100

    Adding my scraper.py file to tracked files

---
+++ scraper.py
@@ -0,0 +1,25 @@
+import pytest
+import asyncio
+from playwright.async_api import async_playwright
+import scrapy
+from scrapy.http import HtmlResponse
+
+
+@pytest.mark.asyncio
+async def test_map():
+	async with async_playwright() as p:
+		browser = await p.chromium.launch()
+
+		need = 'hotels'
+		location = 'Sacramento, CA'
+
+		page = await browser.new_page()
+
+		url = 'https://maps.google.com'
+		await page.goto(url, timeout = 60000)
+
+		search_form = await page.wait_for_selector('form#XmI62e')
+		if search_form:
+			return True
+
+		
